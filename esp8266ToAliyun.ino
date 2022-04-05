6950c9c280c1a8ddbe2d236c500af66eb9a39297
ZhangHaHei <zzsperson@163.com>
2022-04-05 09:53:09 +0800

    fix #1

--- esp8266ToAliyun.ino
+++ esp8266ToAliyun.ino
@@ -1,8 +1,7 @@
 #include <DHT.h>
 #include<ESP8266WiFi.h>
 #define DHTPIN D4 //温度传感器引脚
-#hello
-#world
+#fix
 #define DHTTYPE DHT11 // DHT 11
 DHT dht(DHTPIN, DHTTYPE);
 static WiFiClient espClient;
